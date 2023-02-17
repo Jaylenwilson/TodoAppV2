5f6c5c4dbe3c3f45e5a129d04e84e7d937a6d067
Jaylenwilson <88348326+Jaylenwilson@users.noreply.github.com>
2023-02-16 21:51:59 -0500

    added login route will be doing some changes so a user has the option to login with username or email still have to test the endpoints as well

--- controllers/usercontroller.js
+++ controllers/usercontroller.js
@@ -53,3 +53,60 @@ router.post('/register', async (req, res) => {
 
     }
 })
+
+
+router.post('/login', async (req, res) => {
+    const { email, password } = req.body.user;
+    try {
+        await models.UserModel.findOne({
+            where: {
+                email: email,
+            }
+        })
+            .then(
+                user => {
+                    if (user) {
+                        console.log(password)
+                        console.log(user.password)
+                        bcrypt.compare(password, user.password, (err, matches) => {
+                            if (matches) {
+                                let token = jwt.sign({ id: user.id }, process.env.JWT_SECRET, { expiresIn: 60 * 60 * 24 })
+                                res.status(200).json({
+                                    user: user,
+                                    message: 'logged in sucessfully',
+                                    sessionToken: `Bearer ${token}`
+                                })
+                            } else {
+                                res.status(500).json({
+                                    error: 'incorrect credentials'
+                                })
+                            }
+                        })
+                    } else {
+                        res.status(502).json({
+                            error: 'user does not exist'
+                        })
+                    }
+                }
+            )
+    } catch (err) {
+        if (err.name === 'SequelizeValidationError') {
+            return res.status(400).json({
+                success: false,
+                msg: err.errors.map(e => e.message)
+            })
+        } else if (err instanceof UniqueConstraintError) {
+            res.status(409).json({
+                message: 'Username already in use'
+            })
+        } else {
+            res.status(500).send({
+                message: "sorry could create account"
+            })
+        }
+
+
+
+    }
+})
+
