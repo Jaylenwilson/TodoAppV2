8a0ab299320e480efe2bb9df475b9eae0f719f86
Jaylenwilson <88348326+Jaylenwilson@users.noreply.github.com>
2023-02-18 22:09:12 -0500

    created todo model will read the sequelize doucmentation so I can find the best way to implement dates in the model

--- models/todo.js
+++ models/todo.js
@@ -3,5 +3,24 @@ const { Datatypes } = require('sequelize')
 //  import the connection to our database that we set up in the db.js. This will unlock methods from the sequelize connection that we can call upon.
 const db = require("../db")
 const Todo = db.define("todo", {
-
+    id: {
+        type: Datatypes.UUID,
+        primaryKey: true,
+        defaultValu: Datatypes.UUIDV4,
+        allowNull: false
+    },
+    title: {
+        type: Datatypes.STRING(155),
+        allowNull: false,
+        validate: {
+            notNull: {
+                msg: "Must enter a title"
+            }
+        }
+    },
+    description: {
+        type: Datatypes.STRING(255),
+        allowNull: true
+    },
 })
+module.exports = Todo
