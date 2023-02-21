1aac45a2e45981b48d0bda0b7d3ab941c06215c4
Jaylenwilson <88348326+Jaylenwilson@users.noreply.github.com>
2023-02-20 20:49:08 -0500

    added comments to the headers.js file

--- middleware/headers.js
+++ middleware/headers.js
@@ -1,10 +1,11 @@
+// This middleware sets the appropriate response headers for allowing cross-origin requests
 module.exports = (req, res, next) => {
-    res.header("access-control-allow-origin", "*");
-    res.header("access-control-allow-methods", "GET, POST, PUT, DELETE");
+    res.header("access-control-allow-origin", "*"); // set the value of the Access-Control-Allow-Origin header to '*'
+    res.header("access-control-allow-methods", "GET, POST, PUT, DELETE"); // set the value of the Access-Control-Allow-Methods header to 'GET, POST, PUT, DELETE'
     res.header(
         "access-control-allow-headers",
         "Origin, X-Requested-With, Content-Type, Accept, Authorization"
-    );
+    ); // set the value of the Access-Control-Allow-Headers header to the specified string
 
-    next();
+    next(); // call the next middleware in the chain
 };
