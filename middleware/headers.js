32acdc3434e0688078ab26d9497793cf74ccbc9d
Jaylenwilson <88348326+Jaylenwilson@users.noreply.github.com>
2023-02-20 20:58:06 -0500

    added comments to the validate-session.js file

--- middleware/headers.js
+++ middleware/headers.js
@@ -1,4 +1,13 @@
 // This middleware sets the appropriate response headers for allowing cross-origin requests
+// This is a middleware function that sets the headers for CORS (Cross-Origin Resource Sharing) on incoming HTTP requests. 
+// Here's what each line of the function does:
+// res.header("access-control-allow-origin", "*");: 
+// sets the Access-Control-Allow-Origin header to "*" which means that any website can make requests to this server.
+// res.header("access-control-allow-methods", "GET, POST, PUT, DELETE");: 
+// sets the Access-Control-Allow-Methods header to "GET, POST, PUT, DELETE" which allows these HTTP methods for CORS requests.
+// res.header("access-control-allow-headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization");: 
+// sets the Access-Control-Allow-Headers header to a list of headers that the client is allowed to include in the request.
+// next();: passes control to the next middleware function in the request-response cycle.
 module.exports = (req, res, next) => {
     res.header("access-control-allow-origin", "*"); // set the value of the Access-Control-Allow-Origin header to '*'
     res.header("access-control-allow-methods", "GET, POST, PUT, DELETE"); // set the value of the Access-Control-Allow-Methods header to 'GET, POST, PUT, DELETE'
--- middleware/validate-session.js.js
+++ middleware/validate-session.js.js
@@ -1,16 +1,27 @@
+// Require the jsonwebtoken library
 const jwt = require('jsonwebtoken');
+
+// Import the models object from a separate module
 const { models } = require('../models');
 
+// Define a middleware function to validate JSON web tokens (JWTs)
 const validateJWT = async (req, res, next) => {
+
+    // If the HTTP method is OPTIONS, continue to the next middleware
     if (req.method == 'OPTIONS') {
         next();
-    } else if (req.headers.authorization && req.headers.authorization.includes('Bearer')) {
+    }
+    // Otherwise, if an "Authorization" header is present and includes "Bearer"
+    else if (req.headers.authorization && req.headers.authorization.includes('Bearer')) {
+
+        // Extract the token from the "Authorization" header and verify its signature
         const { authorization } = req.headers;
         const payload = authorization ? jwt.verify(
             authorization.includes('Bearer') ? authorization.split(' ')[1] : authorization,
             process.env.JWT_SECRET
         ) : undefined;
 
+        // If the token is valid, find the user associated with the token's ID
         if (payload) {
             console.log(payload);
             let foundUser = await models.UserModel.findOne({
@@ -19,20 +30,24 @@ const validateJWT = async (req, res, next) => {
                 }
             });
 
+            // If the user is found, add it to the request object and continue to the next middleware
             if (foundUser) {
                 req.user = foundUser;
                 next();
 
+                // Otherwise, return an error response
             } else {
                 res.status(400).send({
                     message: 'Not Authorized'
                 });
             }
+            // If the token is invalid, return an error response
         } else {
             res.status(401).send({
                 message: 'Invalid token'
             });
         }
+        // Otherwise, return an error response
     } else {
         res.status(403).send({
             message: 'Forbidden'
@@ -40,4 +55,5 @@ const validateJWT = async (req, res, next) => {
     };
 };
 
+// Export the validateJWT function for use in other modules
 module.exports = validateJWT;
