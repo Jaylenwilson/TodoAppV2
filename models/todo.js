792121319eea95a97ee613f5a21f0bbdde3c05fe
Jaylenwilson <88348326+Jaylenwilson@users.noreply.github.com>
2023-02-20 20:31:38 -0500

    added a filed for due date where a user has the option to set a due date to a task and added comments to the code

--- models/todo.js
+++ models/todo.js
@@ -1,33 +1,49 @@
-// object destructuring is used to extrapolate the DataTypes object from the sequelize dependency. 
-const { DataTypes } = require('sequelize')
-//  import the connection to our database that we set up in the db.js. This will unlock methods from the sequelize connection that we can call upon.
-const db = require("../db")
-const Todo = db.define("todo", {
+// import DataTypes object from sequelize dependency
+const { DataTypes } = require('sequelize');
+// import connection to database
+const db = require('../db');
+
+// define a Todo model, and set up the fields with their data types and options
+const Todo = db.define('todo', {
+    // id field - primary key, UUID data type with default value generated using UUIDv4, and not nullable
     id: {
         type: DataTypes.UUID,
         primaryKey: true,
         defaultValue: DataTypes.UUIDV4,
         allowNull: false
     },
+    // title field - string data type with maximum length of 155 characters, not nullable, and has a validation rule to ensure it's not null
     title: {
         type: DataTypes.STRING(155),
         allowNull: false,
         validate: {
             notNull: {
-                msg: "Must enter a title"
+                msg: 'Must enter a title'
             }
         }
     },
+    // description field - string data type with maximum length of 255 characters, nullable
     description: {
         type: DataTypes.STRING(255),
         allowNull: true
     },
+    // priority field - integer data type, nullable
     priority: {
-        type: DataTypes.INTEGER
+        type: DataTypes.INTEGER,
+        allowNull: true
     },
+    // completed field - boolean data type with default value of false, nullable
     completed: {
         type: DataTypes.BOOLEAN,
-        defaultValue: false
+        defaultValue: false,
+        allowNull: true
+    },
+    // dueDate field - date-only data type, nullable
+    dueDate: {
+        type: DataTypes.DATEONLY,
+        allowNull: true
     }
-})
-module.exports = Todo
+});
+
+// export Todo model
+module.exports = Todo;
