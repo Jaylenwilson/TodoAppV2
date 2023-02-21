8fef9f3c1bb91caae274d38eba5c931671f91f71
Jaylenwilson <88348326+Jaylenwilson@users.noreply.github.com>
2023-02-20 20:46:03 -0500

    added the option for a user to login using username or email and password to the POST /login endpoint and added comments to my code

--- controllers/usercontroller.js
+++ controllers/usercontroller.js
@@ -1,10 +1,12 @@
-// User can register an account or login t0 an already existing account
+// Importing required packages and modules
 const router = require('express').Router();
 const { models } = require('../models');
 const bcrypt = require('bcryptjs');
 const jwt = require('jsonwebtoken');
 const { UniqueConstraintError } = require('sequelize/lib/errors');
+const { Op } = require('sequelize');
 
+// POST request to create a new user account
 router.post('/register', async (req, res) => {
     const
         {
@@ -13,8 +15,8 @@ router.post('/register', async (req, res) => {
             password
         } = req.body.users
 
-
     try {
+        // Creating a new user using the Sequelize Model
         await models.UserModel.create({
             username: username,
             email: email,
@@ -23,8 +25,7 @@ router.post('/register', async (req, res) => {
             .then(
 
                 user => {
-
-                    console.log(user)
+                    // Creating and returning a JSON Web Token if user is successfully created
                     let token = jwt.sign({ id: user.id }, process.env.JWT_SECRET, { expiresIn: 60 * 60 * 24 });
 
                     res.status(201).json({
@@ -36,79 +37,77 @@ router.post('/register', async (req, res) => {
             )
 
     } catch (err) {
+        // Handling error if username is already in use
         if (err instanceof UniqueConstraintError) {
             res.status(409).json({
                 message: 'Username already in use'
             })
-        } else if (err.name === 'SequelizeValidationError') {
+        }
+        // Handling error if validation error occurs
+        else if (err.name === 'SequelizeValidationError') {
             return res.status(400).json({
                 success: false,
                 msg: err.errors.map(e => e.message)
             })
-        } else {
+        }
+        // Handling other types of errors
+        else {
             res.status(500).json({
                 error: `Sorry we could not create your account ${err}`
             });
         };
-
     }
 })
 
-//  TODO: give the option to enter username or email
-
+// POST request to login with username or email and password
 router.post('/login', async (req, res) => {
-    const { email, password } = req.body.user;
+    const { emailOrUsername, password } = req.body.user;
+
     try {
-        await models.UserModel.findOne({
+        // Querying the database to find a user with email or username provided
+        const user = await models.UserModel.findOne({
             where: {
-                email: email,
+                [Op.or]: [
+                    { email: emailOrUsername },
+                    { username: emailOrUsername }
+                ]
             }
-        })
-            .then(
-                user => {
-                    if (user) {
-                        console.log(password)
-                        console.log(user.password)
-                        bcrypt.compare(password, user.password, (err, matches) => {
-                            if (matches) {
-                                let token = jwt.sign({ id: user.id }, process.env.JWT_SECRET, { expiresIn: 60 * 60 * 24 })
-                                res.status(200).json({
-                                    user: user,
-                                    message: 'logged in sucessfully',
-                                    sessionToken: `Bearer ${token}`
-                                })
-                            } else {
-                                res.status(500).json({
-                                    error: 'incorrect credentials'
-                                })
-                            }
-                        })
-                    } else {
-                        res.status(502).json({
-                            error: 'user does not exist'
-                        })
-                    }
+        });
+
+        if (user) {
+            // Checking if the password provided matches the hashed password in the database
+            bcrypt.compare(password, user.password, (err, matches) => {
+                if (matches) {
+                    // Creating and returning a JSON Web Token if password matches
+                    const token = jwt.sign({ id: user.id }, process.env.JWT_SECRET, { expiresIn: 60 * 60 * 24 });
+                    return res.status(200).json({
+                        user: user,
+                        message: 'Logged in successfully',
+                        sessionToken: `Bearer ${token}`
+                    });
+                } else {
+                    // Returning an error if password does not match
+                    return res.status(500).json({ error: 'Incorrect credentials' });
                 }
-            )
+            });
+        } else {
+            // Returning an error if user does not exist
+            return res.status(502).json({ error: 'User does not exist' });
+        }
     } catch (err) {
+        // Handling error if validation error occurs
         if (err.name === 'SequelizeValidationError') {
             return res.status(400).json({
                 success: false,
                 msg: err.errors.map(e => e.message)
-            })
-        } else if (err instanceof UniqueConstraintError) {
-            res.status(409).json({
-                message: 'Username already in use'
-            })
-        } else {
-            res.status(500).send({
-                message: "sorry could create account"
-            })
+            });
         }
-
-
-
+        // Handling other types of errors
+        else {
+            return res.status(500).send({ message: "Sorry we could not login to your account" });
+        }
     }
-})
+});
 
+// Exporting the router
 module.exports = router
