295b46023a084c8c24438833b6bc5ea91781e670
Jaylenwilson <88348326+Jaylenwilson@users.noreply.github.com>
2023-02-21 20:29:22 -0500

    created the POST /createproject endpoint in the projects controller

--- controllers/projectcontroller.js
+++ controllers/projectcontroller.js
@@ -1,9 +1,40 @@
 // User can create, delete, and update a todo category where todos can be stored
-const router = require('express').Router();
+const Express = require('express');
+const router = Express.Router();
 const { models } = require('../models');
-const bcrypt = require('bcryptjs');
-const jwt = require('jsonwebtoken');
-const { UniqueConstraintError } = require('sequelize/lib/errors');
+const validateJWT = require('../middleware/validate-session.js');
+
+router.post('/projectcreate', validateJWT, async (req, res) => {
+    const { projectName } = req.body.project
+
+    try {
+        await models.ProjectModel.create({
+            projectName: projectName
+        })
+            .then(
+                project => {
+                    res.status(200).send({
+                        project: project,
+                        msg: `${project} has been created`
+                    })
+                }
+            )
+    } catch (err) {
+        // handle validation errors
+        if (err.name === 'SequelizeValidationError') {
+            return res.status(400).json({
+                success: false,
+                msg: err.errors.map(e => e.message)
+            })
+        }
+        else {
+            // handle all other errors
+            res.status(500).json({
+                message: `Sorry we could not create your project ${err}`
+            });
+        };
+    }
+})
 
 
 module.exports = router
